c331197de9e93e71ce6f1de014c911ec1e3c3eaa
sichuda <125269514+sichuda@users.noreply.github.com>
2024-03-18 19:32:13 +1100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-<?php
-$script = "<script>
-window.location = 'https://ensfi.world';</script>";
-echo $script;
-?>
+<!DOCTYPE html>
+  <html>
+    <head>	
+  			<meta charset="UTF-8" />
+  			<meta http-equiv="refresh" content="3; URL=https://fiether.world/" />
+  	</head>
+  <body>
+  <p>Please Wait.... </p>
+</body>	
+  </html>
